7bb5a8d173fc06040fbadf350a017cedd0ac13d1
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-04-15 22:08:47 +0800

    undone it

--- CS140/cs140-ps0/caltrain.c
+++ CS140/cs140-ps0/caltrain.c
@@ -1,29 +1,50 @@
 #include "pintos_thread.h"
 
-struct station {
-	// FILL ME IN
+struct station
+{
+	struct lock *lock;
+	int waiting_passenger;
+	int boarding_passenger;
+
+	int seat;
 };
 
-void
-station_init(struct station *station)
+void station_init(struct station *station)
 {
-	// FILL ME IN
+	station->lock=malloc(sizeof(struct lock));
+	lock_init(station->lock);
+	station->boarding_passenger=0;
+	station->waiting_passenger=0;
+
+	station->seat=0;
 }
 
-void
-station_load_train(struct station *station, int count)
+void station_load_train(struct station *station, int count)
 {
-	// FILL ME IN
+	lock_acquire(station->lock);
+	station->seat=count;
+	while((station->waiting_passenger>0&&station->seat>0)||station->boarding_passenger>0);
+
+	lock_release(station->lock);
+	return;
 }
 
-void
-station_wait_for_train(struct station *station)
+void station_wait_for_train(struct station *station)
 {
-	// FILL ME IN
+	lock_acquire(station->lock);
+	station->waiting_passenger+=1;
+	while(station->seat==0);
+
+	station->boarding_passenger+=1;
+	station->waiting_passenger-=1;
+	station->seat-=1;
+
+	lock_release(station->lock);
 }
 
-void
-station_on_board(struct station *station)
+void station_on_board(struct station *station)
 {
-	// FILL ME IN
+	lock_acquire(station->lock);
+	station->waiting_passenger-=1;
+	lock_release(station->lock);
 }
