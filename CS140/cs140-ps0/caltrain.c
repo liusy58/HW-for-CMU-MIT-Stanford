ae2e73bb79e5ae90ebbc327f113159f26e9cab4b
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-04-16 09:46:00 +0800

    finish caltrain.c

--- CS140/cs140-ps0/caltrain.c
+++ CS140/cs140-ps0/caltrain.c
@@ -3,41 +3,55 @@
 struct station
 {
 	struct lock *lock;
-	int waiting_passenger;
-	int boarding_passenger;
+	struct condition *notfull; //是否还有空位
+	struct condition *full;	   //是否所有的座位都有人坐了
 
-	int seat;
+	int waiting_passenger;	//正在等待的乘客的人数
+	int boarding_passenger; //正在登机的乘客的人数
+
+	int seat;//某趟列车的座位数目
 };
 
 void station_init(struct station *station)
 {
-	station->lock=malloc(sizeof(struct lock));
-	lock_init(station->lock);
-	station->boarding_passenger=0;
-	station->waiting_passenger=0;
+	station->lock = malloc(sizeof(struct lock));
+	station->notfull = malloc(sizeof(struct condition));
+	station->full = malloc(sizeof(struct condition));
 
-	station->seat=0;
+	lock_init(station->lock);
+	cond_init(station->notfull);
+	cond_init(station->full);
+
+	station->boarding_passenger = 0;
+	station->waiting_passenger = 0;
+
+	station->seat = 0;
 }
 
 void station_load_train(struct station *station, int count)
 {
 	lock_acquire(station->lock);
-	station->seat=count;
-	while((station->waiting_passenger>0&&station->seat>0)||station->boarding_passenger>0);
-
+	station->seat = count;
+	while ((station->waiting_passenger > 0 && station->seat > 0) || station->boarding_passenger > 0)
+	{
+		cond_broadcast(station->notfull,station->lock);
+		cond_wait(station->full,station->lock);
+	}
+	station->seat = 0;
 	lock_release(station->lock);
-	return;
+	
 }
 
 void station_wait_for_train(struct station *station)
 {
 	lock_acquire(station->lock);
-	station->waiting_passenger+=1;
-	while(station->seat==0);
+	station->waiting_passenger += 1;
+	while (station->seat == 0)
+		cond_wait(station->notfull,station->lock);
 
-	station->boarding_passenger+=1;
-	station->waiting_passenger-=1;
-	station->seat-=1;
+	station->boarding_passenger += 1;
+	station->waiting_passenger -= 1;
+	station->seat -= 1;
 
 	lock_release(station->lock);
 }
@@ -45,6 +59,9 @@ void station_wait_for_train(struct station *station)
 void station_on_board(struct station *station)
 {
 	lock_acquire(station->lock);
-	station->waiting_passenger-=1;
+	station->boarding_passenger -= 1;
+
+	if((station->seat == 0 || station->waiting_passenger == 0) && station->boarding_passenger == 0)
+		cond_signal(station->full,station->lock);
 	lock_release(station->lock);
 }
