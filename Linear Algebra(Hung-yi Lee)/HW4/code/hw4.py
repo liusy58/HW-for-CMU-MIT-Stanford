a52c0df9bb669874df5640a452c49d9a723d4d6b
Nicholas <liusy58@mail2.sysu.edu.cn>
2020-04-17 09:54:17 +0800

    half finish it

--- Linear Algebra(Hung-yi Lee)/HW4/code/hw4.py
+++ Linear Algebra(Hung-yi Lee)/HW4/code/hw4.py
@@ -2,33 +2,47 @@ import sys
 import numpy as np
 import pandas as pd
 
+
 def load(fname):
     f = open(fname, 'r').readlines()
     n = len(f)
     ret = {}
-    for l in f:
-        l = l.split('\n')[0].split(',')
-        i = int(l[0])
+    for line in f:
+        line = line.split('\n')[0].split(',')
+        i = int(line[0])
         ret[i] = {}
         for j in range(n):
-            if str(j) in l[1:]:
+            if str(j) in line[1:]:
                 ret[i][j] = 1
             else:
                 ret[i][j] = 0
     ret = pd.DataFrame(ret).values
     return ret
 
-def get_tran(g):
-    # TODO
 
-def cal_rank(t, d = 0.85, max_iterations = 1000, alpha = 0.001):
-    # TODO
+def get_tran(graph):
+    graph = np.array(g, dtype=float)
+    nodes = graph.shape[1]
+    for node in range(nodes):
+        non_zero_nbr = np.count_nonzero(graph[:, node])
+        graph[:, node] = graph[:, node] / non_zero_nbr
+    return graph
+
+
+def cal_rank(t, d=0.85, max_iterations=1000, alpha=0.001):
+    line = t.size[0]
+    for _ in range(max_iterations):
+
+
 
 def save(t, r):
-    # TODO
+    np.save("1.txt", t)
+    np.save("2.txt", r)
+
 
 def dist(a, b):
-    return np.sum(np.abs(a-b))
+    return np.sum(np.abs(a - b))
+
 
 def main():
     graph = load(sys.argv[1])
@@ -36,6 +50,6 @@ def main():
     rank = cal_rank(transition_matrix)
     save(transition_matrix, rank)
 
+
 if __name__ == '__main__':
     main()
-
